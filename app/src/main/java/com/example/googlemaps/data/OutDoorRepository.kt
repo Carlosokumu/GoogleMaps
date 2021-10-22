9be19ae717f8e862624d22aae3f5f09878cf497e
Carlosokumu <69672450+Carlosokumu@users.noreply.github.com>
2021-10-22 07:11:02 +0300

    Repository Set Up

---
+++ app/src/main/java/com/example/googlemaps/data/OutDoorRepository.kt
@@ -0,0 +1,18 @@
+package com.example.googlemaps.data
+
+
+import androidx.lifecycle.LiveData
+
+interface OutdoorRepository {
+    fun getAllActivities(): LiveData<List<Activity>>
+
+    fun getAllLocations(): LiveData<List<Location>>
+
+    fun getActivityWithLocations(activityId: Int): LiveData<ActivityWithLocations>
+
+    fun getLocationById(locationId: Int): Location
+
+    fun getLocationWithActivities(locationId: Int): LiveData<LocationWithActivities>
+
+    fun toggleActivityGeofence(id: Int): GeofencingChanges
+}
