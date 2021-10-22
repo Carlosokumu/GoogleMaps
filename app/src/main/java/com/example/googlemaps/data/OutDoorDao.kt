4ea28edac927d834793b45ab10da44efa67b7404
Carlosokumu <69672450+Carlosokumu@users.noreply.github.com>
2021-10-22 07:09:28 +0300

    Dao Set up

---
+++ app/src/main/java/com/example/googlemaps/data/OutDoorDao.kt
@@ -0,0 +1,42 @@
+package com.example.googlemaps.data
+
+
+import androidx.lifecycle.LiveData
+import androidx.room.*
+
+@Dao
+interface OutdoorDao {
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun insertLocations(locations: List<Location>)
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun insertActivities(activities: List<Activity>)
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun insertActivityLocationCrossRefs(activityLocationCrossRefs: List<ActivityLocationCrossRef>)
+
+    @Query("SELECT * FROM Activity ORDER BY title")
+    fun getAllActivities(): LiveData<List<Activity>>
+
+    @Query("SELECT * FROM Location")
+    fun getAllLocations(): LiveData<List<Location>>
+
+    @Transaction
+    @Query("SELECT * FROM Activity WHERE activityId = :activityId")
+    fun getActivityWithLocations(activityId: Int): LiveData<ActivityWithLocations>
+
+    @Query("SELECT * FROM Location WHERE locationId = :locationId")
+    fun getLocationById(locationId: Int): Location
+
+    @Transaction
+    @Query("SELECT DISTINCT L.* FROM Location L, Activity A, ActivityLocationCrossRef AL WHERE L.locationId = AL.locationId AND AL.activityId = A.activityId AND A.geofenceEnabled != 0")
+    fun getLocationsForGeofencing(): List<Location>
+
+    @Transaction
+    @Query("SELECT * FROM Location WHERE locationId = :locationId")
+    fun getLocationWithActivities(locationId: Int): LiveData<LocationWithActivities>
+
+    @Query("UPDATE activity set geofenceEnabled = ~geofenceEnabled WHERE activityId = :id")
+    fun toggleGeofenceEnabled(id: Int): Int
+}
